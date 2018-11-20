6450851383527da335ca5eb6ecc131b22affc185
essential-transformation-0497 <samiraesquina@gmail.com>
2018-11-20 04:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
-function whisper(string){
-  return string.tolowercase()
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log() string.toUpperCase();
+}
+function logWhisper(string){
+  console.log() string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase){
+    return "I love you, too."
+  }
+  }
 }
